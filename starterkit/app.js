9e2336589762b015b7819a5ad109b121da69be3a
Mr.Watcharin Sarachai <watcharin_s@mju.ac.th>
2023-05-03 11:28:57 +0700

    Add base '/app'

--- starterkit/app.js
+++ starterkit/app.js
@@ -1,41 +1,45 @@
 // Imports
 const themesettings = require("./_keenthemes/lib/themesettings.json");
-const dashboardRouter = require("./router/dashboard")
-const authRouter = require("./router/auth")
-const systemRouter = require("./router/system")
-const createKtThemeInstance = require("./_keenthemes/lib/theme")
-const createKtBootstrapInstance = require(`./views/layout/${themesettings.name}/bootstrap`)
-const express = require('express')
-const expressLayouts = require('express-ejs-layouts')
+const dashboardRouter = require("./router/dashboard");
+const authRouter = require("./router/auth");
+const systemRouter = require("./router/system");
+const createKtThemeInstance = require("./_keenthemes/lib/theme");
+const createKtBootstrapInstance = require(`./views/layout/${themesettings.name}/bootstrap`);
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 
 global.themesettings = themesettings;
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 // Static Files
-app.use(express.static('public'))
+app.use("/app", express.static("public"));
 
 // Set Templating Engine
-app.use(expressLayouts)
-app.set('layout', 'layout/master')
-app.set('view engine', 'ejs')
+app.use(expressLayouts);
+app.set("layout", "layout/master");
+app.set("view engine", "ejs");
 
 const init = function (req, res, next) {
-    global.theme = createKtThemeInstance();
-    global.bootstrap = createKtBootstrapInstance();
-    next()
-}
+  global.theme = createKtThemeInstance();
+  global.bootstrap = createKtBootstrapInstance();
+  next();
+};
 
 app.use(init);
 
-app.use('/', dashboardRouter);
-app.use('/auth', authRouter);
-app.use('/system', systemRouter);
+app.use("/app", dashboardRouter);
+app.use("/app/auth", authRouter);
+app.use("/app/system", systemRouter);
 
-app.all('*', (req, res) => {
-    res.status(404).render(theme.getPageViewPath("system", "not-found"), { currentLayout: theme.getLayoutPath("system") });
+app.all("*", (req, res) => {
+  res
+    .status(404)
+    .render(theme.getPageViewPath("system", "not-found"), {
+      currentLayout: theme.getLayoutPath("system"),
+    });
 });
 
 // Listen on Port 3000
-app.listen(port, () => console.info(`App listening on port ${port}`))
+app.listen(port, () => console.info(`App listening on port ${port}`));
